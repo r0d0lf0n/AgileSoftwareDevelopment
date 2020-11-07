140baec61190c56580e500b121aa20650d65fdb4
rodo <fgrrlf81m23z504r@studenti.unical.it>
2020-11-07 16:33:40 +0100

    findAll added

--- banking/src/main/java/it/unical/info/banking/data/dao/BankAccountDao.java
+++ banking/src/main/java/it/unical/info/banking/data/dao/BankAccountDao.java
@@ -4,7 +4,11 @@ import it.unical.info.banking.data.entities.BankAccount;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BankAccountDao extends CrudRepository<BankAccount, Long> {
+
+    List<BankAccount> findAll();
 
 }
