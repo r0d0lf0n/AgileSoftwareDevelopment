44fe45a1daaf0a9dd09d55c508b407932dc04eee
rodo <fgrrlf81m23z504r@studenti.unical.it>
2020-11-07 16:21:14 +0100

    BankAccountTest created

---
+++ banking/src/test/java/it/unical/info/banking/BankAccountTest.java
@@ -0,0 +1,15 @@
+package it.unical.info.banking;
+
+import it.unical.info.banking.data.dao.BankAccountDao;
+import it.unical.info.banking.data.entities.BankAccount;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BankAccountTest {
+
+    @Autowired
+    BankAccountDao bankAccountDao;
+
+
+
+}
