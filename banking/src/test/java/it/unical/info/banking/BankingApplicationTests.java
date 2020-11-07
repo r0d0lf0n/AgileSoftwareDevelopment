603aa5f206594d5e51b5f09b4d1038f425649f6d
rodo <fgrrlf81m23z504r@studenti.unical.it>
2020-11-07 11:14:10 +0100

    boiler-plate
    spring web, JPA-SQL

---
+++ banking/src/test/java/it/unical/info/banking/BankingApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.info.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
