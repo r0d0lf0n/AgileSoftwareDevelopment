a974b6dc61a53175d1e206e49cd8f866d3d21f05
rodo <fgrrlf81m23z504r@studenti.unical.it>
2020-11-08 11:36:25 +0100

    adding plugin for generating metamodel automatically
    <plugin>
    	<groupId>org.codehaus.mojo</groupId>
    	<artifactId>build-helper-maven-plugin</artifactId>
    	<version>3.0.0</version>
    	<executions>
    		<execution>
    			<id>add-source</id>
    			<phase>generate-sources</phase>
    			<goals>
    				<goal>add-source</goal>
    			</goals>
    			<configuration>
    				<sources>
    					<source>target/generated-sources/annotations</source>
    				</sources>
    			</configuration>
    		</execution>
    	</executions>
    </plugin>

---
+++ banking/src/main/java/it/unical/info/banking/helpers/BankAccountSpecificationNew.java
@@ -0,0 +1,20 @@
+package it.unical.info.banking.helpers;
+
+import it.unical.info.banking.data.entities.BankAccount;
+
+import java.time.LocalDate;
+
+public class BankAccountSpecificationNew {
+//    public static Specification<BankAccount> isLongTermCustomer() {
+//        return (root, query, builder) -> {
+//            LocalDate date = LocalDate.now().minusYears(2);
+//            return builder.lessThan(root.get(BankAccount_.ACCOUNT_TYPE), "normal");
+//        };
+//    }
+
+//    public static Specification<BankAccount> hasSalesOfMoreThan(MonetaryAmount value) {
+//        return (root, query, builder) -> {
+//            // build query here
+//        };
+//    }
+}
