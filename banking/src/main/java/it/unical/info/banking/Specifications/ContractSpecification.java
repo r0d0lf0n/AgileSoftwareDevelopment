c9d178d564a21f6c5d0f660b05803796e211801b
rodo <fgrrlf81m23z504r@studenti.unical.it>
2020-11-08 17:46:56 +0100

    contract specification added

---
+++ banking/src/main/java/it/unical/info/banking/Specifications/ContractSpecification.java
@@ -0,0 +1,29 @@
+package it.unical.info.banking.Specifications;
+
+import it.unical.info.banking.data.entities.BankAccount;
+import it.unical.info.banking.data.entities.BankAccount_;
+import it.unical.info.banking.data.entities.Contract;
+import it.unical.info.banking.data.entities.Contract_;
+import it.unical.info.banking.data.helpers.AccountType;
+import org.springframework.data.jpa.domain.Specification;
+
+public class ContractSpecification {
+
+    public static Specification<Contract> hasISIN_Code(Long ISIN_code) {
+        return (root, query, cb) -> {
+            return cb.equal(root.get(Contract_.I_SI_N_CODE), ISIN_code);
+        };
+    }
+
+    public static Specification<Contract> issuedBy(String issuer) {
+        return (root, query, cb) -> {
+            return cb.equal(root.get(Contract_.issuer), issuer);
+        };
+    }
+
+    public static Specification<Contract> hasNominalValue(Long value) {
+        return (root, query, cb) -> {
+            return cb.equal(root.get(Contract_.nominalValue), value);
+        };
+    }
+}
